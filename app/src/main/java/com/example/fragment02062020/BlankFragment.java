1733fbf7e592ef0b9d8322ea070966ca272a7d0b
phatdroid94 <phatdroid94@gmail.com>
2020-09-17 18:50:08 +0700

    add fragment

--- app/src/main/java/com/example/fragment02062020/BlankFragment.java
+++ app/src/main/java/com/example/fragment02062020/BlankFragment.java
@@ -1,51 +1,20 @@
 package com.example.fragment02062020;
 
-import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 public class BlankFragment extends Fragment {
 
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        Log.d("BBB","onAttach");
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (savedInstanceState != null){
-            Log.d("BBB",savedInstanceState.getString("text"));
-        }
-        Log.d("BBB","onCreate");
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("text","xin chao");
-    }
-
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Log.d("BBB","onCreateView");
-        return inflater.inflate(R.layout.fragment_blank, container, false);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.d("BBB","onDestroyView");
+        view = inflater.inflate(R.layout.fragment_blank, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/fragment02062020/MainActivity.java
+++ app/src/main/java/com/example/fragment02062020/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.fragment02062020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnAddFragment;
+    FragmentManager mFragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+
+        mBtnAddFragment = findViewById(R.id.buttonAddFragment);
+        mBtnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                BlankFragment blankFragment = new BlankFragment();
+                fragmentTransaction.add(R.id.linearLayout,blankFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
