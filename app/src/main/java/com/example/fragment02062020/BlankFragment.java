817ce799e5b4a41026dffc8fd94599d9cf9b94b7
phatdroid94 <phatdroid94@gmail.com>
2020-09-17 18:20:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment02062020/BlankFragment.java
@@ -0,0 +1,28 @@
+package com.example.fragment02062020;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment extends Fragment {
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.d("BBB","onAttach");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
